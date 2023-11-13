9e75f25a02a390ea81689c23fb3688b18c492691
Ville Melander <ville.melander@gmail.com>
2023-11-13 17:00:02 +0200

    feat: template for getting hrir with correct angle

--- test_decode.py
+++ test_decode.py
@@ -1,8 +1,32 @@
+import csv
 import numpy as np
 import pathlib
 import spaudiopy as spa  # currently requires python versions >=3.6 but < 3.12
 
 from scipy.io import wavfile
+
+
+def get_hrir(data_path: str):
+    """ Get HRIR at the correct angle based on the angle between the source
+    and the receiver (position data in rx and tx files respectively)
+
+    :param data_path: path to a subject's audio and position files
+    """
+    # Currently positions don't change, so reading the first line is enough to get the positions
+    # todo: movement and quaternions, different heights (?)
+    with open(f'{data_path}/tx_positions.txt', newline='') as source:
+        reader = csv.reader(source, delimiter=' ')
+        src_pos = list(map(float, next(reader)))
+    with open(f'{data_path}/rx_positions.txt', newline='') as receiver:
+        reader = csv.reader(receiver, delimiter=' ')
+        rcv_pos = list(map(float, next(reader)))
+    # Get angle [0, 2*pi[ so that "front" is 0 radians (facing the negative y direction), todo: update (quaternions etc.?)
+    angle = np.arctan2(src_pos[1] - rcv_pos[1], src_pos[0] - rcv_pos[0]) + np.pi / 2
+    if angle < 0:
+        angle = angle + 2 * np.pi
+    #print(f'source: {src_pos[0:2]}, receiver: {rcv_pos[0:2]}, angle: {angle * 180 / np.pi}')
+    # todo: use angle to get the correct hrir and return it, angles might need tweaking (what is front etc.)
+
 
 
 def main():
@@ -20,6 +44,7 @@ def main():
             ambisonic[j, :] = part
         # currently elevation angle will be 0 with the azimuth changing
         # rx file will be the origin, calculate angle with it and tx:s location, and select correct hrir (ineterpolate?)
+        get_hrir(audio_data_path)  # todo
         hrir, _ = spa.io.sofa_to_sh(f'{parent_dir}/data/irs etc/mit_kemar_normal_pinna.sofa', order)  # todo: proper hrir and directivity stuff (?)
 
         binaural = spa.decoder.sh2bin(ambisonic, hrir)
