35391a228af5c7e0de608b2d9f960e773e4189fd
Ville Melander <ville.melander@gmail.com>
2024-01-18 14:46:59 +0200

    style: formatting

--- naf/model/networks.py
+++ naf/model/networks.py
@@ -26,9 +26,8 @@ class KernelLinearAct(nn.Module):
 
 
 class KernelResidualFCEmbeds(nn.Module):
-    def __init__(self, input_ch, intermediate_ch=512, grid_ch=64, num_block=8, output_ch=1, grid_gap=0.25,
-                 grid_bandwidth=0.25, bandwidth_min=0.1, bandwidth_max=0.5, float_amt=0.1, min_xy=None, max_xy=None,
-                 probe=False):
+    def __init__(self, input_ch, intermediate_ch=512, grid_ch=64, num_block=8, output_ch=1, grid_gap=0.25, grid_bandwidth=0.25, bandwidth_min=0.1,
+                 bandwidth_max=0.5, float_amt=0.1, min_xy=None, max_xy=None, probe=False):
         super(KernelResidualFCEmbeds, self).__init__()
         # input_ch (int): number of ch going into the network
         # intermediate_ch (int): number of intermediate neurons
@@ -36,18 +35,13 @@ class KernelResidualFCEmbeds(nn.Module):
         # probe = True returns the features of the last layer
 
         for k in range(num_block - 1):
-            self.register_parameter("left_right_{}".format(k),
-                                    nn.Parameter(torch.randn(1, 1, 1, intermediate_ch) / math.sqrt(intermediate_ch),
-                                                 requires_grad=True))  # test fix
+            self.register_parameter("left_right_{}".format(k), nn.Parameter(torch.randn(1, 1, 1, intermediate_ch) / math.sqrt(intermediate_ch), requires_grad=True))  # test fix
 
         for k in range(4):
-            self.register_parameter("rot_{}".format(k), nn.Parameter(
-                torch.randn(num_block - 1, 1, 1, intermediate_ch) / math.sqrt(intermediate_ch), requires_grad=True))
+            self.register_parameter("rot_{}".format(k), nn.Parameter(torch.randn(num_block - 1, 1, 1, intermediate_ch) / math.sqrt(intermediate_ch), requires_grad=True))
 
         self.proj = BasicProject2(input_ch + int(2 * grid_ch), intermediate_ch)
-        self.residual_1 = nn.Sequential(BasicProject2(input_ch + 128, intermediate_ch),
-                                        nn.LeakyReLU(negative_slope=0.1),
-                                        BasicProject2(intermediate_ch, intermediate_ch))
+        self.residual_1 = nn.Sequential(BasicProject2(input_ch + 128, intermediate_ch), nn.LeakyReLU(negative_slope=0.1), BasicProject2(intermediate_ch, intermediate_ch))
         self.layers = torch.nn.ModuleList()
         for k in range(num_block - 2):
             self.layers.append(KernelLinearAct(intermediate_ch, intermediate_ch))
@@ -69,8 +63,7 @@ class KernelResidualFCEmbeds(nn.Module):
         self.bandwidths = nn.Parameter(torch.zeros(len(grid_coors_x)) + grid_bandwidth, requires_grad=True)
         self.register_buffer("grid_coors_xy", torch.from_numpy(xy_train).float(), persistent=True)
         self.xy_offset = nn.Parameter(torch.zeros_like(self.grid_coors_xy), requires_grad=True)
-        self.grid_0 = nn.Parameter(
-            torch.randn(len(grid_coors_x), grid_ch, device="cpu").float() / np.sqrt(float(grid_ch)), requires_grad=True)
+        self.grid_0 = nn.Parameter(torch.randn(len(grid_coors_x), grid_ch, device="cpu").float() / np.sqrt(float(grid_ch)), requires_grad=True)
 
     def forward(self, input_stuff, rot_idx, sound_loc=None):
         samples = input_stuff.shape[1]
@@ -87,8 +80,7 @@ class KernelResidualFCEmbeds(nn.Module):
 
         my_input = torch.cat((total_grid, input_stuff), dim=-1)
         rot_latent = torch.stack([getattr(self, "rot_{}".format(rot_idx_single)) for rot_idx_single in rot_idx], dim=0)
-        out = self.proj(my_input).unsqueeze(2).repeat(1, 1, 1, 1) + getattr(self, "left_right_0") + rot_latent[:,
-                                                                                                    0]  # tmp
+        out = self.proj(my_input).unsqueeze(2).repeat(1, 1, 1, 1) + getattr(self, "left_right_0") + rot_latent[:, 0]  # tmp
         for k in range(len(self.layers)):
             out = self.layers[k](out) + getattr(self, "left_right_{}".format(k + 1)) + rot_latent[:, k + 1]
             if k == (self.blocks // 2 - 1):
