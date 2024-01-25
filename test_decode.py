165788b3dbf636b09fe8a77b72282a5655d39ba8
Ville Melander <ville.melander@gmail.com>
2024-01-25 12:54:40 +0200

    feat: progress bar and option between dataset and predictions for binauralization

--- test_decode.py
+++ test_decode.py
@@ -3,6 +3,7 @@ import csv
 import numpy as np
 import pathlib
 import spaudiopy as spa  # currently requires python versions >=3.6 but < 3.12
+import tqdm
 
 from scipy.io import wavfile
 
@@ -41,6 +42,7 @@ def parse_input_args():
     parser.add_argument('-o', '--order', default=1, type=int, help='ambisonics order')
     parser.add_argument('-g', '--grid', default='20x10', type=str, help='grid size of the dataset')
     parser.add_argument('--hrir', default='data/hrir/mit_kemar_normal_pinna.sofa', type=str, help='path to the hrir (sofa file) to be used from current parent folder')
+    parser.add_argument('--type', default='dataset', choices=['dataset', 'prediction'], type=str, help='select file structure')
     return parser.parse_args()
 
 
@@ -48,22 +50,33 @@ def main():
     args = parse_input_args()
     order = args.order
     parent_dir = str(pathlib.Path.cwd().parent)
-    save_path = f'{parent_dir}/{args.save_path}/order_{order}'
 
-    data_path_obj = pathlib.Path(f'{parent_dir}/{args.data_path}/ambisonics_{args.order}_{args.grid}/trainset')  # todo: selection for estimated vs. dataset l64
-    subjects = len([p for p in data_path_obj.iterdir() if p.is_dir()])
-    subjects = 20
+    data_path_obj = pathlib.Path(f'{parent_dir}/{args.data_path}/ambisonics_{args.order}_{args.grid}/wav')
+    if args.type == 'dataset':
+        save_path = f'{parent_dir}/{args.save_path}/order_{order}/dataset'
+        data_path_obj = pathlib.Path(f'{parent_dir}/{args.data_path}/ambisonics_{args.order}_{args.grid}/trainset')
+        subjects = len([p for p in data_path_obj.iterdir() if p.is_dir()])
+        subjects = 20  # maybe add this as a parameter when testing dataset
+        progress = tqdm.tqdm(range(1, subjects + 1))  # subject indexing starts from 1 due to how the ML method is set up
+    else:
+        save_path = f'{parent_dir}/{args.save_path}/order_{order}/prediction'
+        progress = tqdm.tqdm(list(data_path_obj.glob('*.wav')))
 
     rm_tree(pathlib.Path(save_path))  # clear old files
     pathlib.Path(save_path).mkdir(parents=True)
 
     hrir, _ = spa.io.sofa_to_sh(f'{parent_dir}/{args.hrir}', order)
-    for i in range(1, subjects + 1):  # subject indexing starts from 1 due to how the ML method is set up
-        audio_data_path = f'{str(data_path_obj)}/subject{i}'
-        fs, ambisonic = wavfile.read(f'{audio_data_path}/ambisonic.wav')
-        # fs, ambisonic = wavfile.read(f'{audio_data_path}.wav')
+    progress.set_description('Binauralizing data')
+    for i in progress:
+        if args.type == 'dataset':
+            fs, ambisonic = wavfile.read(f'{str(data_path_obj)}/subject{i}/ambisonic.wav')
+        else:
+            fs, ambisonic = wavfile.read(f'{i}')
         binaural = spa.decoder.sh2bin(ambisonic.T, hrir)
-        wavfile.write(f'{save_path}/binaural_{i}.wav', fs, binaural.astype(np.float32).T)  # todo: create and name folder
+        if args.type == 'dataset':
+            wavfile.write(f'{save_path}/{args.grid}_s{i}.wav', fs, binaural.astype(np.float32).T)
+        else:
+            wavfile.write(f'{save_path}/{str(i).split("/")[-1]}', fs, binaural.astype(np.float32).T)
 
 
 if __name__ == '__main__':
