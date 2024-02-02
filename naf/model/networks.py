fix: remove rotation parameters

naf/model/networks.py
@@ -39,8 +39,8 @@ class KernelResidualFCEmbeds(nn.Module):
         for k in range(num_block - 1):
             self.register_parameter('left_right_{}'.format(k), nn.Parameter(torch.randn(1, 1, self.components, intermediate_ch) / math.sqrt(intermediate_ch), requires_grad=True))
 
-        for k in range(4):
-            self.register_parameter('rot_{}'.format(k), nn.Parameter(torch.randn(num_block - 1, 1, 1, intermediate_ch) / math.sqrt(intermediate_ch), requires_grad=True))
+        # for k in range(4):
+        #    self.register_parameter('rot_{}'.format(k), nn.Parameter(torch.randn(num_block - 1, 1, 1, intermediate_ch) / math.sqrt(intermediate_ch), requires_grad=True))
 
         self.proj = BasicProject2(input_ch + int(2 * grid_ch), intermediate_ch)
         self.residual_1 = nn.Sequential(BasicProject2(input_ch + 128, intermediate_ch), nn.LeakyReLU(negative_slope=0.1), BasicProject2(intermediate_ch, intermediate_ch))
@@ -81,10 +81,10 @@ class KernelResidualFCEmbeds(nn.Module):
         total_grid = torch.cat((grid_feat_v0, grid_feat_v1), dim=-1).unsqueeze(1).expand(-1, samples, -1)
 
         my_input = torch.cat((total_grid, input_stuff), dim=-1)
-        rot_latent = torch.stack([getattr(self, 'rot_{}'.format(rot_idx_single)) for rot_idx_single in rot_idx], dim=0)
-        out = self.proj(my_input).unsqueeze(2).repeat(1, 1, self.components, 1) + getattr(self, 'left_right_0') + rot_latent[:, 0]
+        # rot_latent = torch.stack([getattr(self, 'rot_{}'.format(rot_idx_single)) for rot_idx_single in rot_idx], dim=0)
+        out = self.proj(my_input).unsqueeze(2).repeat(1, 1, self.components, 1) + getattr(self, 'left_right_0')  # + rot_latent[:, 0]
         for k in range(len(self.layers)):
-            out = self.layers[k](out) + getattr(self, 'left_right_{}'.format(k + 1)) + rot_latent[:, k + 1]
+            out = self.layers[k](out) + getattr(self, f'left_right_{k + 1}')  # + rot_latent[:, k + 1]
             if k == (self.blocks // 2 - 1):
                 out = out + self.residual_1(my_input).unsqueeze(2).repeat(1, 1, self.components, 1)
         if self.probe: